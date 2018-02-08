99b0e5ddc1aee58bb5222e9a00006ee5e4e14d3d
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2018-02-08 12:52:39 +0300

    Home Activity and its Layout Implemented

--- app/src/main/java/mustafaozhan/github/com/livewallpaper/MainActivity.kt
+++ app/src/main/java/mustafaozhan/github/com/livewallpaper/MainActivity.kt
@@ -1,12 +1,18 @@
 package mustafaozhan.github.com.livewallpaper
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, HomeActivity::class.java)).apply { finish() }
+        }, 3000)
     }
 }
