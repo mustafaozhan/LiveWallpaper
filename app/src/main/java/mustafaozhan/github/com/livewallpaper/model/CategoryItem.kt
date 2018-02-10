9d939fbadd15b86086863cabfb5b70e28f285ddd
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2018-02-10 12:58:35 +0300

    Model implemented and Firebase table implemented

---
+++ app/src/main/java/mustafaozhan/github/com/livewallpaper/model/CategoryItem.kt
@@ -0,0 +1,6 @@
+package mustafaozhan.github.com.livewallpaper.model
+
+/**
+ * Created by Mustafa Ozhan on 2/10/18 at 12:56 PM on Arch Linux wit Love <3.
+ */
+data class CategoryItem(val name:String?=null,val imageLink:String?=null)
