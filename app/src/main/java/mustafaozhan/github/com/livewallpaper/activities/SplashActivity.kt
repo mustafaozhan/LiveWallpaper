3b47fc408e9a5b7e64b9bab21a8da72062b672fb
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2018-02-18 18:58:53 +0300

    Designing Splash Screen

--- app/src/main/java/mustafaozhan/github/com/livewallpaper/activities/SplashActivity.kt
+++ app/src/main/java/mustafaozhan/github/com/livewallpaper/activities/SplashActivity.kt
@@ -4,16 +4,23 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
 import mustafaozhan.github.com.livewallpaper.R
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
             startActivity(Intent(this, HomeActivity::class.java)).apply { finish() }
-        }, 3000)
+        }, 5000)
     }
 }
