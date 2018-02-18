ffbe31e5e39c80a9602e5e8dce9dc5b945d375de
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2018-02-18 19:12:32 +0300

    Listing Activity Created

---
+++ app/src/main/java/mustafaozhan/github/com/livewallpaper/activities/ListActivity.kt
@@ -0,0 +1,13 @@
+package mustafaozhan.github.com.livewallpaper.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import mustafaozhan.github.com.livewallpaper.R
+
+class ListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
